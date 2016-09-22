34a46dd5fc9c08370916102a83e0899aec3f20ab
dance2die <dontbothermewithspam@gmail.com>
2016-09-22 06:36:16 -0400

    added ScrollSpy

--- ResponsivePatterns/02/scrolling.js
+++ ResponsivePatterns/02/scrolling.js
@@ -2,4 +2,9 @@
 	$(window).on('load resize', function () {
 		$('.fill-screen').css('height', window.innerHeight);
 	});
+
+	// add Bootstrap's scrollspy
+	$('body').scrollspy({
+		target: '.navbar'
+	});
 });
